b5058d0f6d2619d47300287388687cc77bf883a0
onlyyasad <onlyyasad@gmail.com>
2023-06-10 22:11:31 +0600

    api for feedback successfully generated

--- index.js
+++ index.js
@@ -192,6 +192,16 @@ async function run() {
             res.send(result)
         })
 
+        app.patch("/classes/feedback/:id", async (req, res) => {
+            const id = req.params.id;
+            const feedback = req.body.feedback;
+            console.log(feedback)
+            const query = { _id: new ObjectId(id) };
+            const update = { $set: { feedback: feedback } };
+            const result = await classesCollection.updateOne(query, update);
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
