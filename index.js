a77192e38b302b5584d482f769748b3b8d5a8546
onlyyasad <onlyyasad@gmail.com>
2023-06-12 23:30:24 +0600

    completed

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const usersCollection = client.db('creativeCampersDB').collection('users');
         const classesCollection = client.db('creativeCampersDB').collection('classes');
@@ -98,7 +98,7 @@ async function run() {
         app.patch("/users/role/:email", async (req, res) => {
             const email = req.params.email;
             const role = req.body.role;
-            console.log(email, role)
+            
             const query = { email: email };
             const update = { $set: { role: role } };
             const result = await usersCollection.updateOne(query, update);
@@ -123,7 +123,7 @@ async function run() {
             const result = await usersCollection.find(query).toArray();
             res.send(result)
         })
-        
+
         app.get('/users/instructor/:email', verifyJwt, async (req, res) => {
             const email = req.params.email;
 
@@ -158,10 +158,10 @@ async function run() {
             res.send(result)
         })
 
-        app.post('/classes', verifyJwt, verifyInstructor, async (req, res) => {
-            const newClass = req.body;
-            const result = await classesCollection.insertOne(newClass);
-            res.send(result);
+        app.get('/classes/popular', async (req, res) => {
+            const filter = { status: "approved" };
+            const result = await classesCollection.find(filter).sort({enrolled: -1}).limit(6).toArray();
+            res.send(result)
         })
 
         app.get('/myClasses', verifyJwt, async (req, res) => {
@@ -191,10 +191,16 @@ async function run() {
             res.send(result)
         })
 
+        app.post('/classes', verifyJwt, verifyInstructor, async (req, res) => {
+            const newClass = req.body;
+            const result = await classesCollection.insertOne(newClass);
+            res.send(result);
+        })
+
         app.patch("/classes/status/:id", async (req, res) => {
             const id = req.params.id;
             const status = req.body.status;
-            console.log(id, status)
+            
             const query = { _id: new ObjectId(id) };
             const update = { $set: { status: status } };
             const result = await classesCollection.updateOne(query, update);
@@ -204,7 +210,7 @@ async function run() {
         app.patch("/classes/feedback/:id", async (req, res) => {
             const id = req.params.id;
             const feedback = req.body.feedback;
-            console.log(feedback)
+            
             const query = { _id: new ObjectId(id) };
             const update = { $set: { feedback: feedback } };
             const result = await classesCollection.updateOne(query, update);
@@ -272,7 +278,7 @@ async function run() {
         app.post("/create-payment-intent", async (req, res) => {
             const { price } = req.body;
             const amount = price * 100;
-            console.log(price, amount)
+            
             // Create a PaymentIntent with the order amount and currency
             const paymentIntent = await stripe.paymentIntents.create({
                 amount: amount,
